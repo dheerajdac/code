31d5b2757cdd6f3b949117e6d59fd2ecbab1e330
dheerajdac <dheerajdac@gmail.com>
2020-09-22 20:46:23 +0530

    commit container

---
+++ src/main/java/com/dheerajdac/code/leet/medium/Permutations.java
@@ -0,0 +1,19 @@
+package com.dheerajdac.code.leet.medium;
+
+import java.util.List;
+
+public class Permutations {
+
+    public List<List<Integer>> permute(int[] nums) {
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        Permutations permutations = new Permutations();
+        permutations.permute(nums);
+
+        
+    }
+}
